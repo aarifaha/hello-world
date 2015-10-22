3cb58b92e1611b200e44ba7498bd83e1b2aacab4
aarifaha <aarifaha@ryerson.ca>
2015-10-22 18:36:29 -0400

    added a new line

---
+++ hello world/src/hello world.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : hello.cpp
+// Author      : Anshuman Arif
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Hello World!! Hello Brien";
+
+	return 0;
+}
